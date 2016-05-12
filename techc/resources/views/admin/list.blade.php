d830897d52e45e1354268f27f4df8fc1ede19863
mikanyo-guruto <kasugataisyou@yahoo.co.jp>
2016-05-12 15:00:58 +0900

    listのデザインを変更

--- techc/resources/views/admin/list.blade.php
+++ techc/resources/views/admin/list.blade.php
@@ -2,41 +2,51 @@
 <html>
 	<head>
 		<link href="{{{asset('/assets/css/bootstrap.min.css')}}}" rel="stylesheet">
+		<link href="{{{asset('/assets/css/list.css')}}}" rel="stylesheet">
 		<meta charset="UTF-8">
 	</head>
 
 <body>
-	<button type="button" class="btn btn-primary">作品アンケート</button>
-	<button type="button" class="btn btn-success">会場アンケート</button>
-	<h2>作品別</h2>
-	<table class="table table-bordered">
-		<thead>
-			<tr>
-				<th>作品名</th>
-				<th>作者名</th>
-				<th>評価数</th>
-				<th>詳細</th>
-			</tr>
-		</thead>
-		<tbody>
-		@foreach($lists as $list)
-			<tr>
-				<td>{{{ $list->product_name }}}</td>
-				<td>
-					<span>
-					@foreach($list->member as $member)
-						{{ $member->name }}
-					@endforeach
-					</span>
-				</td>
-				<td>
-					{{ $sum_val[$list->id] }}
-				</td>
-				<td><a href="./detail/{{ $list->id }}" class="btn btn-info">詳細</a></td>
-			</tr>
-		@endforeach
-		</tbody>
-	</table>
+	<div class="wrap">
+		<div class="side_bar">
+			<h2>We are TECH.C<br>アンケートシステム</h2>
+			<ul>
+				<li><a href="#">作品アンケート</a></li>
+				<li>会場アンケート</li>
+			</ul>
+		</div>
+		<div class="list">
+			<h2>作品別</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>作品名</th>
+						<th>作者名</th>
+						<th>評価数</th>
+						<th>詳細</th>
+					</tr>
+				</thead>
+				<tbody>
+				@foreach($lists as $list)
+					<tr>
+						<td>{{{ $list->product_name }}}</td>
+						<td>
+							<span>
+							@foreach($list->member as $member)
+								{{ $member->name }}
+							@endforeach
+							</span>
+						</td>
+						<td>
+							{{ $sum_val[$list->id] }}
+						</td>
+						<td><a href="./detail/{{ $list->id }}" class="btn btn-info">詳細</a></td>
+					</tr>
+				@endforeach
+				</tbody>
+			</table>
+		</div>
+	</div>
 </body>
 
 </html>

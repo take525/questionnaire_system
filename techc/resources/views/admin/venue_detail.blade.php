b8dcd6d902390d41f249d2de54f4ed0bd7924b08
mikanyo-guruto <kasugataisyou@yahoo.co.jp>
2016-05-17 11:52:26 +0900

    会場アンケート仮mock完成

---
+++ techc/resources/views/admin/venue_detail.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link href="{{{asset('/assets/css/bootstrap.min.css')}}}" rel="stylesheet">
+<link href="{{{asset('/assets/css/venue_detail.css')}}}" rel="stylesheet">
+</head>
+
+<body>
+<div class="wrap">
+	<div class="info">
+		<h2>佐藤太郎さん<small>のアンケート内容</small></h2><p>2015/01/01</p>
+		<hr>
+		<table class="info_table">
+			<tr>
+				<th>1.生徒の対応は良かったですか</th>
+				<td>とてもよかった</td>
+			</tr>
+			<tr>
+				<th>1.生徒の対応は良かったですか</th>
+				<td>とてもよかった</td>
+			</tr>
+			<tr>
+				<th>1.生徒の対応は良かったですか</th>
+				<td>とてもよかった</td>
+			</tr>
+			<tr>
+				<th>1.生徒の対応は良かったですか</th>
+				<td>とてもよかった</td>
+			</tr>
+			<tr>
+				<th>1.生徒の対応は良かったですか</th>
+				<td>とてもよかった</td>
+			</tr>
+		</table>
+
+		<table class="info_supplement">
+			<tr>
+				<th>1.インターンのご予定はありますか</th>
+				<td>ある</td>
+			</tr>
+			<tr>
+				<th>1.インターンのご予定はありますか</th>
+				<td>ある</td>
+			</tr>
+			<tr>
+				<th>1.インターンのご予定はありますか</th>
+				<td>ある</td>
+			</tr>
+		</table>
+	</div>
+</div>
+</body>
+
+</html>
